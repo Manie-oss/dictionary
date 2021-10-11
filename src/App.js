b8503248b25ead18410f4506f2b76e106029291a
Manie-oss <to.mansip@gmail.com>
2021-10-11 18:06:33 +0530

    fixed the output for changed word

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import './App.css';
 function App(){
 
   const [word, setWord] = useState('');
+  const [previousWord, setPreviousWord] = useState('');
   const [data, setData] = useState('')
 
   function searchWord(){
+    setPreviousWord(word);
     Axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     .then((response) => {
       setData(response.data);
@@ -29,15 +31,15 @@ function App(){
         <input type = "text" placeholder = "Search" onChange = {(e) => {setWord(e.target.value)}} />
         <button className = "button" onClick = { searchWord }><FaSearch size="23px" /></button>
       </div>
-      {data && 
+      {data && (word !== '' && word === previousWord  &&
       (<div className = "output"> 
         <div className = "show-data"><span style = {{color: "green"}}>Word: </span>{word}
           <button style = {{ marginLeft: "100px"}} onClick = { pronounceWord }><FcSpeaker size="23px" style = {{color: "green"}}/></button>
-        </div>
-        <div className = "show-data"><span style = {{color: "green"}}>Part of speech: </span>{data[0].meanings[0].partOfSpeech}</div>
-        <div className = "show-data"><span style = {{color: "green"}}>Meaning: </span>{data[0].meanings[0].definitions[0].definition}</div>
-        <div className = "show-data"><span style = {{color: "green"}}>Example: </span>{data[0].meanings[0].definitions[0].example}</div>
-      </div>)}
+        </div> 
+          <div className = "show-data"><span style = {{color: "green"}}>Part of speech: </span>{data[0].meanings[0].partOfSpeech}</div>
+          <div className = "show-data"><span style = {{color: "green"}}>Meaning: </span>{data[0].meanings[0].definitions[0].definition}</div>
+          <div className = "show-data"><span style = {{color: "green"}}>Example: </span>{data[0].meanings[0].definitions[0].example}</div>
+      </div>))}
       
     </div>
   )
